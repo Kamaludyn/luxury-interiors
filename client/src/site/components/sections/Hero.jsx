e2f86a4474f40866b0684e5728df8a2a348c09ae
kamaludyn <kamaluddeen305@gmail.com>
2025-12-01 21:41:25 +0100

    feat(hero-ui): add hero section image carousel

--- client/src/site/components/sections/Hero.jsx
+++ client/src/site/components/sections/Hero.jsx
@@ -1,9 +1,53 @@
+import { useState, useEffect, useRef } from "react";
 import { motion, useScroll, useTransform } from "framer-motion";
-import { useRef } from "react";
-import hero from "../../../assets/imgs/hero.jpg";
+import { CaretLeft, CaretRight } from "phosphor-react";
+import hero1 from "../../../assets/imgs/hero.jpg";
+import hero2 from "../../../assets/imgs/hero-shelves.jpg";
+import hero3 from "../../../assets/imgs/drywall-partition.jpeg";
+
+const heroImages = [
+  {
+    title: "Redefining Luxury",
+    subTitle: "Transforming spaces with elegance and comfort",
+    image: hero1,
+  },
+  {
+    title: "Visionary Creations",
+    subTitle: "Your vision, beautifully brought to life.",
+    image: hero2,
+  },
+  {
+    title: "Inspired Design",
+    subTitle: "Elevate your living through inspired design",
+    image: hero3,
+  },
+];
 
 const Hero = () => {
+  const [currentIndex, setCurrentIndex] = useState(0);
   const ref = useRef(null);
+
+  // Go to the next hero image in the carousel
+  const handleNext = () => {
+    setCurrentIndex((prevIndex) => (prevIndex + 1) % heroImages.length);
+  };
+
+  // Go to the previous image in the carousel
+  const handlePrev = () => {
+    setCurrentIndex(
+      (prevIndex) => (prevIndex - 1 + heroImages.length) % heroImages.length
+    );
+  };
+
+  // Transition to the next image every 5 seconds
+  useEffect(() => {
+    const timer = setTimeout(handleNext, 5000);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [currentIndex]);
+
   const { scrollYProgress } = useScroll({
     target: ref,
     offset: ["start start", "end start"],
@@ -12,14 +56,29 @@ const Hero = () => {
   return (
     <section
       ref={ref}
-      className="relative min-h-screen py-24 flex items-center justify-center overflow-hidden bg-background-500 text-text-500"
+      className="relative group min-h-screen py-24 flex items-center justify-center overflow-hidden bg-background-500 text-text-500"
     >
       <motion.div style={{ y }} className="absolute inset-0 z-0">
         <img
-          src={hero}
+          src={heroImages[currentIndex].image}
+          // src={hero}
           alt="Luxury Interior Background"
           className="w-full h-full object-cover opacity-90"
         />
+        <div className="flex justify-between w-[95%] absolute top-[40%] left-[2.5%] md:opacity-0 group-hover:opacity-100 transition-opacity duration-300">
+          <CaretLeft
+            size={40}
+            weight="bold"
+            className="bg-surface-500/70 text-primary-500 rounded-full text-center text-4xl cursor-pointer shadow-lg z-50"
+            onClick={handlePrev}
+          />
+          <CaretRight
+            size={40}
+            weight="bold"
+            className="bg-surface-500/70 text-primary-500 rounded-full text-center text-4xl cursor-pointer shadow-lg z-50"
+            onClick={handleNext}
+          />
+        </div>
         <div className="absolute inset-0 bg-linear-to-t from-black/40 to-transparent dark:from-black/60"></div>
       </motion.div>
 
@@ -29,11 +88,13 @@ const Hero = () => {
         className="relative z-10 text-center px-6 mt-auto md:mt-0 md:mx-auto"
       >
         <h1 className="w-fit mx-auto bg-primary-500 text-surface-600 px-6 py-2 text-2xl md:text-3xl lg:text-5xl font-semibold dark:text-accent-800 rounded-4xl leading-tight animate-fadeIn">
-          Redefining Luxury
+          {heroImages[currentIndex].title}
+          {/* Redefining Luxury */}
           {/* <br /> in Modern Home Design */}
         </h1>
         <p className="bg-surface-500 py-2 px-4 -mt-2 text-lg md:text-xl text-primaary-500 max-w-2xl mx-auto rounded-4xl leading-relaxed z-50">
-          Transforming spaces with elegance and comfort
+          {heroImages[currentIndex].subTitle}
+          {/* Transforming spaces with elegance and comfort */}
         </p>
       </motion.div>
     </section>
